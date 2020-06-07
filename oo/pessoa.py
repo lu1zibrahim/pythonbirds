f2e6f1a021897fa4ca210a6174210fc893fecb28
lu1zibrahim <luizibrahim94@gmail.com>
2020-06-07 18:58:30 -0300

    atualização dos testes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,8 +17,14 @@ class Pessoa():
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+
+
+class Homem(Pessoa):
+    pass
+
+
 if __name__ == '__main__':
-    luiz = Pessoa(nome='Luiz')
+    luiz = Homem(nome='Luiz')
     carlos = Pessoa(luiz,nome="Carlos")
     print(Pessoa.cumprimentar(carlos))
     print(id(carlos))
@@ -40,3 +46,8 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos),id(carlos.olhos), id(luiz.olhos))
     print(Pessoa.metodo_estatico(), luiz.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(),luiz.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa,Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(luiz, Pessoa))
+    print(isinstance(luiz, Homem))
