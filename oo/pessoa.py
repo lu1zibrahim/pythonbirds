08bf7eb0d0dd3f75166d8d6dcbea45702a2dc4b9
lu1zibrahim <luizibrahim94@gmail.com>
2021-03-03 09:27:53 -0300

    Criado atributo dinâmico e removido atributo de dado

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,5 @@
 class Pessoa:
-    def __init__(self,*filhos, nome = None, idade=26):
+    def __init__(self, *filhos, nome=None, idade=26):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -9,15 +9,18 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    luiz = Pessoa(nome="Luiz")
+    luiz = Pessoa(nome="Otavio")
     pai = Pessoa(luiz, nome="Carlos", idade=74)
     print(Pessoa.cumprimentar(pai))
     print(id(pai))
     print(pai.cumprimentar())
     print(pai.nome)
-    pai.nome = "Luiz Carlos"
-    print(pai.nome)
     print(pai.idade)
     for filho in pai.filhos:
         print(filho.nome)
-    print(pai.filhos)
+    pai.sobrenome = "Ibrahim"
+    print(pai.sobrenome)
+    del pai.filhos
+    print(pai.__dict__)
+    print(luiz.__dict__)
+
